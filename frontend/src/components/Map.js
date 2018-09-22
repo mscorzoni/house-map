9fc9fae9ff7fc538c030f80fc2ae59f21851d50f
Marcos Scorzoni <mlessa@gmail.com>
2018-09-22 11:42:08 +0100

    add map component

---
+++ frontend/src/components/Map.js
@@ -0,0 +1,15 @@
+import React, { Components } from 'react';
+
+export class Map extends Component {
+  render() {
+    return(
+        <div>
+          Map
+        </div>
+      );
+  }
+}
+
+export default Map
+
+
