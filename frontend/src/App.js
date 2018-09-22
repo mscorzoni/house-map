6566c3c9acd16eed4f8bef14bfcc5edd06a531bb
Marcos Scorzoni <mlessa@gmail.com>
2018-09-22 13:52:01 +0100

    add place window component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Map } from './components/Map'
 
 class App extends Component {
   render() {
--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,15 +1,49 @@
-import React, { Components } from 'react';
+import React, { Component } from 'react';
+import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { PlaceMarker } from './PlaceMarker';
+
+
+const AirbnbMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultCenter={props.center}
+    defaultZoom={props.zoom}>
+    {props.places}
+    </GoogleMap>
+));
+
 
 export class Map extends Component {
+  constructor(props) {
+    super(props)
+ 
+    this.zoom = 7
+ 
+    this.state = {
+      lat: 50.0515918,
+      lng: 19.9357531
+    };
+  }
+ 
   render() {
+    const {lat, lng} = this.state;
+    const places = [<PlaceMarker lat={lat} lng={lng} price={20}/>]
+ 
     return(
-        <div>
-          Map
-        </div>
-      );
+      <div style={{width: `750px`, height: `750px`}}>
+        <AirbnbMap
+          center={{
+            lat: lat,
+            lng: lng
+          }}
+          zoom={this.zoom}
+          containerElement={
+            <div style={{ height: `100%` }} />
+          }
+          mapElement={
+            <div style={{ height: `100%` }} />
+          }
+        />
+      </div>
+    );
   }
 }
-
-export default Map
-
-
